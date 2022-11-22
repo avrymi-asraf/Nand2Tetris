bd30dc9df3d5096ad51ad9570242f6f5e5ab65d8
AviyaMeshiMalca <aviyamalca@gmail.com>
2022-11-22 17:10:00 +0200

    boot

---
+++ 08/Codes.py
@@ -0,0 +1,332 @@
+
+class Codes:
+    #TODO delete lines
+
+    push_constant =  """
+//**push constant**
+@index
+D = A
+
+@SP
+A = M
+M = D
+
+@SP
+M = M+1"""
+
+    push_segment =  """
+//**push argument**
+//go to segment at index
+
+@segment
+D = M
+
+@index
+D = D+A
+
+A = D
+D = M
+
+// write in SP
+@SP
+A = M
+M = D
+
+// inc SP
+@SP
+M = M+1"""
+
+    push_static = """
+//**push static**
+//go to static at the symbol "Xxx.i". write it in the top of the stack 
+
+@index
+D = M
+
+// write in SP
+@SP
+A = M
+M = D
+
+// inc SP
+@SP
+M = M+1"""
+
+    pop_segment = """ 
+//**pop argument**
+
+//find address
+@segment
+D = M
+
+@index
+D = D + A
+
+@_temp_index
+M = D
+
+//take data from top of stack
+@SP
+A = M - 1 
+D = M
+
+@_temp_index
+A = M
+M = D
+
+//decrement SP
+@SP
+M = M-1
+
+"""
+
+    pop_static = """ 
+//**pop static**
+
+//take data from top of stack
+@SP
+A = M -1
+D = M
+
+//write data inside segment at the given index
+@index
+M = D
+
+//decrement SP
+@SP
+M = M-1
+
+"""
+
+    C_add = """
+//**add**
+@SP
+AM=M-1
+D=M
+
+@SP 
+AM = M-1
+M = D+M
+
+@SP
+M =M+1"""
+
+    C_sub = """
+//add
+@SP
+AM=M-1
+D=M
+
+@SP 
+AM = M-1
+M = M-D
+
+@SP
+M =M+1"""
+
+    C_neg = """ 
+@SP
+A = M-1
+M = -M
+"""
+   
+    C_eq = """
+@SP
+AM = M-1
+D = M   //old value
+A = A-1
+D = D-M // the difference
+@eq._counter
+D;JEQ       //if is not equal         
+@SP          //else
+A = M-1
+M = 0
+@end._counter
+0;JMP
+(eq._counter) //set to true  
+    @SP
+    A = M-1
+    M = -1
+(end._counter)
+"""  
+  
+    C_lt = """
+@SP
+AM = M-1
+D = M
+@Yltz._counter
+D;JLT            
+@SP       
+A = M-1
+D = M
+@true._counter 
+D;JLT          
+(sub._counter)  
+@SP
+A = M
+D = D-M
+@false._counter
+D;JGE
+@true._counter
+0;JMP
+(Yltz._counter)
+@SP
+A = M-1
+D = M
+@false._counter
+D;JGE
+@sub._counter
+0;JMP
+
+(true._counter)
+@SP
+A = M-1
+M = -1
+@end._counter
+0;JMP
+
+(false._counter)
+@SP
+A = M-1
+M = 0
+
+(end._counter) 
+""" 
+    
+    C_gt = """
+@SP
+AM = M-1
+D = M
+@Yltz._counter
+D;JLT            
+@SP       
+A = M-1
+D = M
+@false._counter 
+D;JLT          
+(sub._counter)  
+@SP
+A = M
+D = D-M
+@true._counter
+D;JGT
+@false._counter
+0;JMP
+(Yltz._counter)
+@SP
+A = M-1
+D = M
+@true._counter
+D;JGE
+@sub._counter
+0;JMP
+
+(true._counter)
+@SP
+A = M-1
+M = -1
+@end._counter
+0;JMP
+
+(false._counter)
+@SP
+A = M-1
+M = 0
+
+(end._counter) 
+"""
+    
+    C_and = """
+@SP
+AM = M-1
+D = M
+
+@SP
+A = M-1
+M = D&M
+"""
+    
+    C_or= """
+//OR  
+@SP
+AM = M-1
+D = M
+
+@SP
+A = M-1
+M = D|M
+    """
+   
+    C_not = """
+@SP
+A = M-1
+M = !M
+    """
+
+    C_shiftleft = """
+@SP
+A = M-1
+M = M<<
+"""
+
+    C_shiftright = """
+@SP
+A = M-1
+M = M>>
+"""
+
+    push_this_that ="""
+     //**push this that**
+@index
+D = M
+
+@SP         
+A=M
+M = D
+
+@SP
+M=M+1
+    """
+
+    pop_this_that ="""
+    //**pop this that**
+@SP
+A = M-1      
+D = M
+
+@index
+M = D
+
+@SP
+M = M-1
+"""
+
+
+    pop_temp = """ 
+    //**pop temp**
+@SP
+A = M-1      
+D = M
+
+@Rnew_index
+M = D
+
+@SP
+M = M-1      
+
+    """
+    
+    push_temp ="""
+    //**push temp**
+
+@Rnew_index
+D = M
+
+@SP
+A = M     
+M = D
+
+@SP
+M = M+1 
+
+    """
--- 08/Main.py
+++ 08/Main.py
@@ -15,6 +15,7 @@ from CodeWriter import CodeWriter
 def translate_file(
         input_file: typing.TextIO, output_file: typing.TextIO,
         bootstrap: bool) -> None:
+
     """Translates a single file.
 
     Args:
@@ -23,8 +24,54 @@ def translate_file(
         bootstrap (bool): if this is True, the current file is the 
             first file we are translating.
     """
-    # Your code goes here!
-    pass
+
+    parser = Parser(input_file)
+    code_writer = CodeWriter(output_file)
+    
+    input_filename, input_extension = os.path.splitext(os.path.basename(input_file.name))
+
+    code_writer.set_file_name(input_filename)
+
+    if bootstrap : 
+        pass
+        #  code_writer.init
+
+    while parser.has_more_commands:
+        
+        if parser.command_type() == Command.C_ARITHMETIC:
+            code_writer.write_arithmetic(parser.arg1())
+        
+        elif parser.command_type() in ( Command.C_PUSH, Command.C_POP ):
+            code_writer.write_push_pop(parser.command_type() ,parser.arg1(), parser.arg2())
+        
+        elif parser.command_type() == Command.C_LABEL:
+            pass
+        
+        elif parser.command_type() == Command.C_GOTO:
+            pass
+        
+        elif parser.command_type() == Command.C_IF:
+            pass
+        
+        elif parser.command_type() == Command.C_FUNCTION:
+            pass
+        
+        elif parser.command_type() == Command.C_RETURN:
+            pass
+        
+        elif parser.command_type() == Command.C_CALL:
+            pass
+
+
+        else:
+            raise Exception("Unknown command {} ", parser.command_type())    
+
+        parser.advance()
+
+
+
+
+
 
 
 if "__main__" == __name__:
@@ -36,6 +83,7 @@ if "__main__" == __name__:
     if not len(sys.argv) == 2:
         sys.exit("Invalid usage, please use: VMtranslator <input path>")
     argument_path = os.path.abspath(sys.argv[1])
+    # the input path is a directory to some files
     if os.path.isdir(argument_path):
         files_to_translate = [
             os.path.join(argument_path, filename)
@@ -55,3 +103,53 @@ if "__main__" == __name__:
             with open(input_path, 'r') as input_file:
                 translate_file(input_file, output_file, bootstrap)
             bootstrap = False
+
+
+
+"""
+This file is part of nand2tetris, as taught in The Hebrew University, and
+was written by Aviv Yaish. It is an extension to the specifications given
+[here](https://www.nand2tetris.org) (Shimon Schocken and Noam Nisan, 2017),
+as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0
+Unported [License](https://creativecommons.org/licenses/by-nc-sa/3.0/).
+"""
+
+import os
+import sys
+import typing
+from Parser import Parser
+from CodeWriter import CodeWriter
+import Command
+from Command import Command
+
+
+
+if "__main__" == __name__:
+    # Parses the input path and calls translate_file on each input file.
+    # This opens both the input and the output files!
+    # Both are closed automatically when the code finishes running.
+    # If the output file does not exist, it is created automatically in the
+    # correct path, using the correct filename.
+    if not len(sys.argv) == 2:  
+        sys.exit("Invalid usage, please use: VMtranslator <input path>")
+    argument_path = os.path.abspath(sys.argv[1])
+    if os.path.isdir(argument_path):
+        #does not get here
+        files_to_translate = [
+            os.path.join(argument_path, filename)
+            for filename in os.listdir(argument_path)]
+        output_path = os.path.join(argument_path, os.path.basename(
+            argument_path))
+    else:
+        
+        files_to_translate = [argument_path]
+        output_path, extension = os.path.splitext(argument_path)
+    output_path += ".asm"
+    with open(output_path, 'w') as output_file:
+        for input_path in files_to_translate:
+            filename, extension = os.path.splitext(input_path)
+            if extension.lower() != ".vm":
+                continue
+            with open(input_path, 'r') as input_file:
+                translate_file(input_file, output_file)
+    
